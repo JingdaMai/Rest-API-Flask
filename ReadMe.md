6d85445d87e6e94c334eecd849aaea64b049148d
Jingda Mai <datsmai@hotmail.com>
2020-03-20 16:03:09 +0900

    use sqlalchemy

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,6 @@
 # Description
 
-A RESTful Flask API with sqlite.
+A RESTful API Web Server with ORM using Flask, JWT, and SQLAlchemy
 
 
 
@@ -12,19 +12,35 @@ A RESTful Flask API with sqlite.
 
 - **GET**
 
-  [/item/<string: name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#759709e2-dd46-44ee-9604-af3d006e1c36)
+  [/item/<string:name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#759709e2-dd46-44ee-9604-af3d006e1c36)
 
 - **POST**
 
-  [/item/<string: name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#fd3008e7-3592-4234-9c6c-09ff7c239d30)
+  [/item/<string:name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#fd3008e7-3592-4234-9c6c-09ff7c239d30)
 
 - **PUT**
 
-  [/item/<string: name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#b70c2200-b724-4c44-ad32-5127fe81fb5f)
+  [/item/<string:name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#b70c2200-b724-4c44-ad32-5127fe81fb5f)
 
 - **DEL**
 
-  [/item/<string: name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#05b94b55-e777-44bc-ac34-080d8e337357)
+  [/item/<string:name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#05b94b55-e777-44bc-ac34-080d8e337357)
+
+- **GET**
+
+  [/stores](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#e3ef5b24-ace5-4ac6-9e51-213e77b70fbf)
+
+- **GET**
+
+  [/store/<string:name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#eeb169d9-0a70-4776-9d2e-0ed937c76aec)
+
+- **POST**
+
+  [/store/<string:name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#bba5794f-d793-412b-be9e-8a974e942700)
+
+- **DEL**
+
+  [/store/<string:name>](https://documenter.getpostman.com/view/5788993/SzS7P62U?version=latest#7eca68c0-e072-4f9f-878c-6e740aa84c76)
 
 - **POST**
 
--- resources/item.py
+++ resources/item.py
@@ -1,6 +1,5 @@
 from flask_restful import Resource, reqparse
 from flask_jwt import jwt_required
-import sqlite3
 from models.item import ItemModel
 
 
@@ -12,6 +11,11 @@ class Item(Resource):
                         type=float,
                         required=True,
                         help="This field cannot be left blank!"
+                        )
+    parser.add_argument('store_id',
+                        type=int,
+                        required=True,
+                        help="Every item needs a store id."
                         )
 
     @jwt_required()
@@ -27,10 +31,10 @@ class Item(Resource):
 
         data = self.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, **data)
 
         try:
-            item.insert()
+            item.save_to_db()
         except:
             return {"message": "An error occurred inserting the item."}, 500
 
@@ -38,47 +42,27 @@ class Item(Resource):
 
     @jwt_required()
     def delete(self, name):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "DELETE FROM items WHERE name=?"
-        cursor.execute(query, (name,))
-
-        connection.commit()
-        connection.close()
-
-        return {'message': 'Item deleted'}
+        item = ItemModel.find_by_name(name)
+        if item:
+            item.delete_from_db()
+            return {'message': 'item deleted'}
+        return {'message': 'item not exists'}, 400
 
     @jwt_required()
     def put(self, name):
         data = self.parser.parse_args()
         item = ItemModel.find_by_name(name)
-        updated_item = ItemModel(name, data['price'])
+
         if item is None:
-            try:
-                updated_item.insert()
-            except:
-                return {"message": "An error occurred inserting the item."}, 500
+            item = ItemModel(name, **data)
         else:
-            try:
-                updated_item.update()
-            except:
-                return {"message": "An error occurred updating the item."}, 500
-        return updated_item.json()
+            item.price = data['price']
+
+        item.save_to_db()
+
+        return item.json()
 
 
 class ItemList(Resource):
-    TABLE_NAME = 'items'
-
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * FROM {table}".format(table=self.TABLE_NAME)
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-        connection.close()
-
-        return {'items': items}
+        return {'items': [item.json for item in ItemModel.query.all()]}
